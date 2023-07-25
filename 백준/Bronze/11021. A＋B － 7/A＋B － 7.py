057866cd105a41b336e841ece1da6442eed2a063
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-07-25 16:12:28 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,8 @@
+def fastAB():
+    import sys
+    T = int(sys.stdin.readline().rstrip())
+    for i in range(T):
+        a, b = map(int,sys.stdin.readline().rstrip().split())
+        res = a+b
+        print(f'Case #{i+1}: {res}')
+fastAB()
