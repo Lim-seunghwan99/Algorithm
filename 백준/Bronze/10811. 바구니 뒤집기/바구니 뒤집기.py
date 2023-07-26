a33f1d2425dd5eba28d6ee2920d2974d6a396878
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-07-26 17:51:11 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -3,18 +3,14 @@ def ball_3():
     t = [i for i in range(1, N+1)]
     for _ in range(M):
         i, j = list(map(int, input().split()))
-        if (j-i) <= 1:
-            k = t.pop(j-1)
-            t.insert(i-1, k)
-        else:# 21435
-            w = t[i-1:j] # k = 2 1435
-            a = list(reversed(w))
-            d = t[:i-1]
-            c = t[j:]
-            t = []
-            t.extend(d)
-            t.extend(a)
-            t.extend(c)
+        w = t[i-1:j] # k = 2 1435
+        a = list(reversed(w))
+        d = t[:i-1]
+        c = t[j:]
+        t = []
+        t.extend(d)
+        t.extend(a)
+        t.extend(c)
 
     print(*t)
 ball_3()
