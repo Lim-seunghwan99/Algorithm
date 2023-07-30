10eb3f61aa40906b5878548b3669ae39b3d0752d
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-07-31 00:13:26 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,14 +1,5 @@
-def readwords():
-    word_arr = [['*' for j in range(15)]for i in range(5)]
-
-    for i in range(5):
-        word_list = input()
-        for idx, word in enumerate(word_list):
-            word_arr[i][idx] = word
-
-    for j in range(15):
-        for i in range(5):
-            if word_arr[i][j] == '*':
-                continue
-            print(word_arr[i][j], end = '')
-readwords()
+A = [input() for i in range(5)]
+for i in range(15):
+    for j in range(5):
+        if i < len(A[j]):
+            print(A[j][i], end='')
