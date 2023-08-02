827748a65d799154eb6a90149d1e8a6595caa932
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-03 00:33:20 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,10 +1,10 @@
 def snail():
     A, B, V = list(map(int, input().split()))
-    end = V - B
-    x = end / (A-B)
-    if x == int(x):
-        print(int(x))
-    else:
-        print(int(x) + 1)
+    end = V - A
+    cnt = end // (A-B)
+    if end % (A-B) > 0:
+        cnt += 1
+    cnt += 1
+    print(cnt)
 
 snail()
