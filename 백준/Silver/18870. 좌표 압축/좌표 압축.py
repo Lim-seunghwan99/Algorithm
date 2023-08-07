ba99a2ad00f22a1376546976f1f5ab4a6a39f904
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-07 16:24:24 +0900

    [Silver II] Title: 좌표 압축, Time: 1980 ms, Memory: 154808 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,21 @@
+def cordinate():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    t = []
+    t = list(sorted(set(arr)))
+    d = {t[i]:i for i in range(len(t))}
+    for i in arr:
+        print(d[i], end=' ')
+    #t = list(t)
+
+    #for k in range(N):
+    #    print(t.index(arr[k]), end=' ')
+    # arr = 2 4 - 10 4 -9
+    # arr을 정렬하면
+    # -10 -9 2 4 4
+    # -9보다 작은 값은 1
+    # 자기보다 작은 값을 가진다
+    # 2 3 0 3 1
+    # arr = 1000 999 1000 999 1000 999
+    # 결과값 1 0 1 0 1 0 중복부분 없애야 한다
+cordinate()
