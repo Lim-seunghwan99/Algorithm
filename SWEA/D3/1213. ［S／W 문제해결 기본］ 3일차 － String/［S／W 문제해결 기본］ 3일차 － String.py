b67cac1127185341e1cf31e92a149db8cd45aadb
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-08 09:38:51 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 147 ms, Memory: 58,488 KB -BaekjoonHub

---
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -0,0 +1,10 @@
+def swea1214():
+    for tc in range(1, 11):
+        N = int(input())
+        a = input()
+        b = input()
+        c = b.split(a)
+        print(f'#{N} {len(c)-1}')
+
+
+swea1214()
