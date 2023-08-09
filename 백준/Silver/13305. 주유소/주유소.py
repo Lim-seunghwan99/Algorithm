3fa48cf6dec6b3109d229f74336ee0a385ae9f51
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-10 00:02:48 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 47204 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,22 +1,13 @@
-def gasstation():
-    N = int(input())
-    arr = list(map(int, input().split())) # 도로의 길이 키로당 1리터
-    pr = list(map(int, input().split())) # 리터당 가격
-    sv = 0
-    cur = 0
-    walk = 0
-    i = 0
-    while cur < N-1:
-        if pr[cur+i] >= pr[cur+1+i]:
-            walk += arr[cur+i]
-            sv += walk * pr[cur]
-            cur += i + 1
-            i = 0
-            walk = 0
-        else:
-            walk += arr[cur]
-            i += 1
-    print(sv)
+def gasstation2():
+    n = int(input())
+    arr = list(map(int, input().split()))  # 도로의 길이 키로당 1리터
+    pr = list(map(int, input().split()))  # 리터당 가격
 
-
-gasstation()
+    res = 0
+    m = pr[0]
+    for i in range(n-1):
+        if pr[i] < m:
+            m = pr[i]
+        res += m * arr[i]
+    print(res)
+gasstation2()
