e8f19a65e173538ea4b0bec2a83e1cdb85ec2006
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-11 23:22:28 +0900

    [Gold V] Title: 탑, Time: 532 ms, Memory: 118508 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -5,13 +5,13 @@ def top():
     res = [0] * N
 
     for i, num in enumerate(arr):
-        while stack and stack[-1][1] <= num:
+        while stack and stack[-1][1] <= num: # num보다 한단계 큰 값을 찾을 때 까지 계속 pop
             stack.pop()
 
-        if stack:
-            res[i] = stack[-1][0]
+        if stack: # 스택이 있으면 레이저가 걸린다는 뜻
+            res[i] = stack[-1][0] # 스택의 가장 큰 값의 인덱스를 반환
 
-        stack.append((i + 1, num))
+        stack.append((i + 1, num)) # 스택에 계속 넣는다 내림차순으로 비교하기 위해서
 
     print(*res, sep=' ')
 
