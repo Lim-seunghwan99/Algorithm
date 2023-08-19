18f171511306fdf527256e66bc778b5ae61cb4b0
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-19 16:13:31 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,29 @@
 import sys
 sys.setrecursionlimit(10000)
-
-def dfs(x, y):
+# 재귀에러 발생시
+def check(i, j):
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
-    
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if (0 <= nx < m) and (0 <= ny < n):
-            if field[ny][nx] == 1:
-                field[ny][nx] = 0
-                dfs(nx, ny)
-                
-for _ in range(int(sys.stdin.readline())):
-    m, n, k = map(int, sys.stdin.readline().split())
-    field = [[0 for _ in range(m)] for _ in range(n)]
-    count = 0
-    for _ in range(k):
-        x, y = map(int, sys.stdin.readline().split())
-        field[y][x] = 1
-    for x in range(m):
-        for y in range(n):
-            if field[y][x] == 1:
-                dfs(x, y)
-                count += 1
-    print(count)
+    for k in range(4):
+        ni = i + dx[k]
+        nj = j + dy[k]
+        if 0 <= ni < M and 0 <= nj < N and arr[nj][ni] == 1: # 범위 M이다** # 범위 안에 있는 경우
+            arr[nj][ni] = 0
+            check(ni, nj)
+
+
+T = int(sys.stdin.readline())
+for tc in range(1, T+1):
+    M, N, K = map(int, sys.stdin.readline().split())
+    arr = [[0] * M for _ in range(N)]
+    for _ in range(K):
+        i, j = map(int, sys.stdin.readline().split())
+        arr[j][i] = 1
+    res = 0
+    for i in range(M):
+        for j in range(N):
+            if arr[j][i] == 1:# 1을 찾고, 방문하지 않았다면,
+                check(i, j)
+                res += 1
+
+    print(res)
