b6516c03e72476df652ca360ba54452df4d54985
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-20 22:54:58 +0900

    [Silver IV] Title: 카드2, Time: 260 ms, Memory: 55092 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,8 @@
+from collections import deque
+N = int(input())
+arr = [i for i in range(1, N+1)]
+deq = deque(arr)
+while len(deq) > 1:
+    deq.popleft()
+    deq.append(deq.popleft())
+print(deq[0])
