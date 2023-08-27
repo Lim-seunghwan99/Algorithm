a46cbdc6d5078f5d39adbd7b9cb0794165813fb4
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-27 22:22:54 +0900

    [Silver II] Title: 스타트와 링크, Time: 1164 ms, Memory: 123416 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,30 +1,27 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-board = [list(map(int,input().split())) for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 visited = [False for _ in range(N)]
-INF = 2147000000
-res = INF
+inf = 200000000
+res = inf
 
-# ✨ DFS
-def DFS(L,idx):
+def dfs(L, idx):
     global res
     if L == N//2:
-        A = 0
-        B = 0
+        a = 0
+        b = 0
         for i in range(N):
             for j in range(N):
                 if visited[i] and visited[j]:
-                    A += board[i][j]
+                    a += arr[i][j]
                 elif not visited[i] and not visited[j]:
-                    B +=board[i][j]
-        res = min(res, abs(A-B))
-        return
-    for i in range(idx,N):
+                    b += arr[i][j]
+        res = min(res, abs(a-b))
+    for i in range(idx, N):
         if not visited[i]:
             visited[i] = True
-            DFS(L+1,i+1)
+            dfs(L+1, i+1)
             visited[i] = False
-            
-DFS(0,0)
+dfs(0, 0)
 print(res)
