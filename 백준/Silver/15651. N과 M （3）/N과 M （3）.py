fa692b7915781c57b1790f6b3c2c7ccc6d489315
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-08-27 16:36:08 +0900

    [Silver III] Title: N과 M (3), Time: 884 ms, Memory: 156300 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+def find(cur):
+    if len(s) == m:
+        print(*s)
+        return
+    for i in range(1, n+1):
+        s.append(i)
+        find(i+1)
+        s.pop()
+
+
+n, m = map(int, input().split())
+arr = [i for i in range(n)]
+s = []
+find(1)
