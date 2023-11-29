056e47e9020d9d28b033f283957952813bb0333c
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-11-29 16:54:03 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 264 ms, Memory: 119440 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+board = [list(map(int, input())) for i in range(N)]
+dx = [0, -1, -1]
+dy = [-1, 0, -1]
+max_num = 0
+for i in range(len(board)):
+    for j in range(len(board[i])):
+        bp = 0
+        if board[i][j] == 1:
+            mx = 1e9
+            for k in range(3):
+                nx = i + dx[k]
+                ny = j + dy[k]
+                if 0 <= nx < len(board) and 0 <= ny < len(board[i]):
+                    if mx > board[nx][ny]:
+                        mx = board[nx][ny]
+                else:
+                    bp = 1
+                    break
+            if bp == 0:
+                board[i][j] = mx + 1
+        if max_num < board[i][j]:
+            max_num = board[i][j]
+
+print(max_num ** 2)
