61e399b341bed0ba288cdd74e146913c030232d2
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-12-18 20:41:48 +0900

    [level unrated] Title: n의 배수 고르기, Time: 2.13 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+        return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+        // return answer;
+    }
+}
