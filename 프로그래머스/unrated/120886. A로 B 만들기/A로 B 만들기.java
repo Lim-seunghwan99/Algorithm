5383c5a08d081cd6a3687414a4de26540cf75020
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2023-12-19 23:39:05 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.52 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String before, String after) {
+        char[] a = after.toCharArray();
+        char[] b = before.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return new String(a).equals(new String(b)) ? 1 : 0;
+    }
+}
