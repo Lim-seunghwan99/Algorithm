2aa599ed2f705e80c9b6b678c5b01d4e5fa2afb5
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2024-01-27 11:18:18 +0900

    [D3] Title: 이진수 표현, Time: 206 ms, Memory: 33,664 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,25 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int lastNBit = (1 << (N)) - 1;  // 111..1 (길이 N)
+            if( lastNBit == (M & lastNBit)) {
+                System.out.println("#" + test_case + " " + "ON");
+            } else {
+                System.out.println("#" + test_case + " " + "OFF");
+            }
+		}
+	}
+}
