fc1cd7f37900f560843da6b2f5756ec038bce793
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2025-02-27 20:54:53 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -1,14 +1,15 @@
-def trianglememorize():
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    if a == b == c == 60:
-        print('Equilateral')
-    elif a + b + c == 180 and a == b or a == c or b == c:
-        print('Isosceles')
-    elif a + b + c == 180 and a != b != c:
-        print('Scalene')
+# 세 각의 크기가 모두 60이면, Equilateral
+# 세 각의 합이 180이고, 두 각이 같은 경우에는 Isosceles
+# 세 각의 합이 180이고, 같은 각이 없는 경우에는 Scalene
+# 세 각의 합이 180이 아닌 경우에는 Error
+# 를 출력하는 프로그램을 작성하시오.
+a, b, c = [int(input()) for i in range(3)]
+if a == b == c == 60:
+    print("Equilateral")
+elif a + b + c == 180:
+    if a == b or b == c or a == c:
+        print("Isosceles")
     else:
-        print('Error')
-
-trianglememorize()
+        print("Scalene")
+else:
+    print("Error")
