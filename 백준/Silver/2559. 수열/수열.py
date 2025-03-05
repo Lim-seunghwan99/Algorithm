2ecd1d48acda5d7931ec415da21fd46c53589cdf
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2025-03-05 13:00:58 +0900

    [Silver III] Title: 수열, Time: 112 ms, Memory: 121500 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,11 +1,11 @@
 N, K = map(int, input().split())
 days = list(map(int, input().split()))
-
-sv = sum(days[:K])  # 첫 K일의 합
-ans = sv  # 최댓값을 첫 구간 합으로 초기화
+sv = sum(days[:K])
+ans = sv
 
 for i in range(K, N):
-    sv = sv - days[i - K] + days[i]  # 슬라이딩 윈도우 적용
-    ans = max(ans, sv)  # 최댓값 갱신
-
+    # sv에서 이전 값 빼고, 이후 값 추가
+    sv = sv - days[i - K] + days[i]
+    if ans < sv:
+        ans = sv
 print(ans)
