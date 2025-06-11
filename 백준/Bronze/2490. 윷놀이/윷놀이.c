da08d923351e1c95cfbcff86b476fb5fe2951f30
Lim-seunghwan99 <139419039+Lim-seunghwan99@users.noreply.github.com>
2025-06-11 12:57:15 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.c
+++ 백준/Bronze/2490. 윷놀이/윷놀이.c
@@ -1,31 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void Yogurt()
+
+void BOJ2490()
 {
-    int i, j;
-    int N;
-    int cnt;
-    for (i = 0; i < 3; i++)
-    {
-        cnt = 0;
-        for (j = 0; j < 4; j++)
-        {
-            scanf("%d", &N);
-            if (N == 0)
-                cnt++;
-        }
-        if (cnt == 0)
-            printf("E\n");
-        else if (cnt == 1)
-            printf("A\n");
-        else if (cnt == 2)
-            printf("B\n");
-        else if (cnt == 3)
-            printf("C\n");
-        else
-            printf("D\n");
-    }
+	int num, sv_cnt;
+
+	for (int i = 0; i < 3; i++) {
+		sv_cnt = 0;
+		for (int j = 0; j < 4; j++) {
+			scanf("%d", &num);
+			if (num == 0)
+				sv_cnt++;
+		}
+		switch (sv_cnt) {
+		case 0:
+			printf("E\n");
+			break;
+		case 1:
+			printf("A\n");
+			break;
+		case 2:
+			printf("B\n");
+			break;
+		case 3:
+			printf("C\n");
+			break;
+		case 4:
+			printf("D\n");
+			break;
+		}
+	}
+
+	return 0;
 }
-int main()
-{
-    Yogurt();
+
+int main() {
+	BOJ2490();
 }
